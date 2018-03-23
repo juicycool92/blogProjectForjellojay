b64561cdd324f2e9689dab56435b4aa3ca8daa4c
juicycool92 <juicycool92@gmail.com>
2018-03-24 00:05:29 +0900

    add simple log

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieSession = require('cookie-session') ;
 app.set('views',path.resolve(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,"static")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('trust proxy', 1) // trust first proxy
+app.use(cookieSession({
+    name : 'mySession',
+    keys : ['myKey1','myKey2']
+}));
 
 app.listen(8080,()=>{
     console.log('server ist up');
 });
 
 
+app.use((req,res,next)=>{
+    var remoteIp = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+        remoteIp = remoteIp.split(':').slice(-1)[0];
+    if(remoteIp == '1')
+        remoteIp = 'localhost';
 
+    var today = new Date();
+    today = today.setMinutes(0,0,0);
+    if(!req.session.lastVisit | req.session.lastVisit != today){
+        req.session.lastVisit = today;
+        console.log('time stamp changed!!');
+        var logStr = '[INFO]['+remoteIp+']['+new Date()+']access/visit counted';
+    }else{
+        var logStr = '[INFO]['+remoteIp+']['+new Date()+']access';
+    }
+    
+    fs.writeFile('./log.txt',logStr,'utf8',(err,data)=>{
+        if(err){
+            console.log('error on write log');
+        }
+    });
+    next();
+});
 var blogModule = require('./modules/blogModule.js');
 var codeModule = require('./modules/codeModule.js');
 var jsonCreator = require('./modules/jsonCreator.js');
@@ -23,3 +56,4 @@ var board = require('./routes/board.js')(app,blogModule,codeModule,jsonCreator);
 var blog = require('./routes/blog.js')(app,blogModule,jsonCreator);
 var code = require('./routes/code.js')(app,codeModule,jsonCreator);
 var reply = require('./routes/reply.js')(app,replyModule,jsonCreator);
+
