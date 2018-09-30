b8b3d833b872f44716fc222c7da74677c0febda5
Jay <juicycool92@gmail.com>
2018-09-30 23:48:16 +0900

    desktop weekend
    does it work? no clue

--- webServer/static/js/face.js
+++ webServer/static/js/face.js
@@ -9,22 +9,98 @@ const _ALERT = {
         "TRUE":"alert-success","FALSE":"alert-danger","INFO":"alert-info"
     },
     "TYPE" : {
-        "LOGIN":0,"SIGN":1,"FACEAUTH":2
+        "LOGIN":0,"SIGN":1,"FACEAUTH":2,"FACEAPPEND":3
     }
 };
 const _BTN = {
-    "COLOR" : {
-        "BLUE" : "btn-primary",
-        "GRAY" : "btn-secondary",
-        "GREEN" : "btn-success",
-        "RED" : "btn-danger",
-        "YELLOW" : "btn-warning",
-        "CYAN" : "btn-info",
-        "WHITE" : "btn-light",
-        "BLACK" : "btn-dark",
-        "LINK" : "btn-link"
+    COLOR : {
+        BLUE : "btn-primary",
+        GRAY : "btn-secondary",
+        GREEN : "btn-success",
+        RED : "btn-danger",
+        YELLOW : "btn-warning",
+        CYAN : "btn-info",
+        WHITE : "btn-light",
+        BLACK : "btn-dark",
+        LINK : "btn-link"
     },
-    "LENGTH" : 9
+    NAME : {
+        UPDATE :'btn_updateFaceAuth',
+        REGIST : 'btn_registFaceAuth',
+        TOGGLE_UPDATE : 'btn_toggleUpdate',
+    },
+    LENGTH : 9
+}
+const _MSG = {
+    UPDATEFACE : "face camera!",
+    CAMAUTHFAILED : "camera is not ready or permission not grant!",
+    UPDATESTOPPED : "update stopped"
+}
+function deleteAlertClass(targetObject){
+    if(targetObject.classList.contains(_ALERT.STATUS.TRUE)){
+        targetObject.classList.remove(_ALERT.STATUS.TRUE);
+    }
+    if(targetObject.classList.contains(_ALERT.STATUS.FALSE)){
+        targetObject.classList.remove(_ALERT.STATUS.FALSE);
+    }
+    if(targetObject.classList.contains(_ALERT.STATUS.INFO)){
+        targetObject.classList.remove(_ALERT.STATUS.INFO);
+    }
+}
+function alertSetting(alertType = null, isVisiable = false, status = _ALERT.STATUS.FALSE, message = null,target = null,targetMsg = null){
+    switch(alertType){
+        case _ALERT.TYPE.LOGIN : {
+            deleteAlertClass(loginSign[0]);                
+            loginSign[0].classList.add(status);
+            isVisiable ? ( loginSign[0].classList.contains('in') ? null : loginSign[0].classList.add('in') ) : ( loginSign[0].classList.contains('in') ? loginSign[0].classList.remove('in') : null );
+            if(message){
+                loginSign[1].innerText = message;
+            }
+            break;
+        }
+        case _ALERT.TYPE.SIGN : {
+            deleteAlertClass(signSign[0]);                
+            signSign[0].classList.add(status);
+            isVisiable ? ( signSign[0].classList.contains('in') ? null : signSign[0].classList.add('in') ) : ( signSign[0].classList.contains('in') ? signSign[0].classList.remove('in') : null );
+            if(message){
+                signSign[1].innerText = message;
+            }
+            break;
+        }
+        case _ALERT.TYPE.FACEAUTH : {
+            deleteAlertClass(authSign[0]);                
+            authSign[0].classList.add(status);
+            isVisiable ? ( authSign[0].classList.contains('in') ? null : authSign[0].classList.add('in') ) : ( authSign[0].classList.contains('in') ? authSign[0].classList.remove('in') : null );
+            if(message){
+                authSign[1].innerText = message;
+            }
+            break;
+        }
+        case _ALERT.TYPE.FACEAPPEND : {
+            target.classList.add(status);
+            isVisiable ? ( target.classList.contains('in') ? null : target.classList.add('in') ) : ( target.classList.contains('in') ? authSign[0].classList.remove('in') : null );
+            if(message){
+                targetMsg.innerText = message;
+            }
+            break;
+        }
+        default : {
+            throw new Error('[ERROR] alertType is null, wrong args givin');
+        }
+    }
+}
+function alertToggle(isVisiable,alertId){
+    if(isVisiable==true){
+        document.getElementById(''+alertId+'').classList.add('in') ;
+    }else if(isVisiable==false){
+        document.getElementById(''+alertId+'').classList.remove('in') ;
+    }else if(isVisiable==null){
+        if(document.getElementById(''+alertId+'').classList.contains('in')){
+            document.getElementById(''+alertId+'').classList.remove('in') ;
+        }else{
+            document.getElementById(''+alertId+'').classList.add('in') ;
+        }
+    }
 }
 
 const getheringCurImage = ()=>{
@@ -43,6 +119,14 @@ const setBtnColor = (btn,target,dest)=>{
     }
 }
 $(function(){
+    $.fn.killCam = ()=>{
+        if(!mediaDevices){
+            mediaDevices=null;
+        }else{
+            mediaDevices.getVideoTracks()[0].stop();
+            mediaDevices=null;
+        }
+    }
     $.fn.initCam = (selectedVideo)=>{
         video = selectedVideo;
         constraints = {
@@ -64,23 +148,29 @@ $(function(){
                     context.height = video.videoHeight;
                     video.play();
                     const sendImg = getheringCurImage();
+                    video.setAttribute("playsinline", true);
                     return true;
                    
                 }
             }).catch(e =>{
                 switch(e.name){
-                    case "NotAllowedError":{
+                    case 'NotAllowedError':{
+                        alert(e);
                         alertSetting(_ALERT.TYPE.FACEAUTH,true,_ALERT.STATUS.FALSE,"You must allow cam for Face Auth");
                         break;
                     }
+                    case 'NotFoundError' : {
+                        alert('NO CAMERA DETECTING');break;
+                    }
                     default : {
+                        alert(e);
                         alertSetting(_ALERT.TYPE.FACEAUTH,true,_ALERT.STATUS.FALSE,"Unknown error!!!");
                         break;
                     }
                 }   
                 return false;
             });
-        video.setAttribute("playsinline", true);
+//        video.setAttribute("playsinline", true);
     };
 
     $.fn.ajaxCall_faceAuth = (userId,userImg)=>{
